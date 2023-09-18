f5f5621d715d7ea0217fb1d4bcc4335c1148ec2c
lucasomori <lomori@berkeley.edu>
2023-09-18 11:26:09 -0700

    Initial Commit

---
+++ lecture demo/lecture_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  lecture_demoApp.swift
+//  lecture demo
+//
+//  Created by Lucas Omori on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct lecture_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
